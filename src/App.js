44db86a5a293762617e4c80371374f19b9da0735
Here Perry <heremoana.perry@epsi.fr>
2022-11-14 18:30:37 +0100

    nav ok

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import PokemonDetailsView from './views/PokemonDetailsView/PokemonDetailsView';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MyPokemonView from './views/MyPokemonView/MyPokemonView';
-
-const Stack = createNativeStackNavigator();
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const App = () => {
   const HomeStack = createNativeStackNavigator();
@@ -47,7 +47,28 @@ const App = () => {
   return (
     <>
       <NavigationContainer>
-        <Tab.Navigator screenOptions={{headerShown: false}}>
+        <Tab.Navigator
+          screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+              let iconName;
+
+              if (route.name === 'Home') {
+                return <FontAwesomeIcon name="home" size={30} color={'blue'} />;
+              } else if (route.name === 'MyPokemon') {
+                return (
+                  <MaterialCommunityIcons
+                    name="pokeball"
+                    size={size}
+                    color={color}
+                  />
+                );
+              }
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}>
           <Tab.Screen name="Home" component={HomeStackScreen} />
           <Tab.Screen name="MyPokemon" component={MyPokemonStackScreen} />
         </Tab.Navigator>
